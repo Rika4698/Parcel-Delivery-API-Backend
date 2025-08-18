7c52c8268c9ecc6e66fc83bcd44584c7bd3c0130
Rika4698 <rikasharmin181@gmail.com>
2025-08-19 01:05:02 +0600

    create error handler and not found

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { notFound } from "./app/middlewares/notFound";
 
 
 const app = express()
@@ -13,6 +15,9 @@ app.get("/", (req:Request, res:Response)=>{
     });
 
 });
+
+app.use(globalErrorHandler)
+app.use(notFound)
 
 
 export default app;
