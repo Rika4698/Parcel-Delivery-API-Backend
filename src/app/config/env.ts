388fa0686df386cdf82a84446d7beb5196d8adfa
Rika4698 <rikasharmin181@gmail.com>
2025-09-03 16:01:07 +0600

    change

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -46,7 +46,7 @@ const loadEnvVariables = (): EnvConfig =>{
         BCRYPT_SALT_ROUND:Number(process.env.BCRYPT_SALT_ROUND),
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
-        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_Secret as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
         JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         ADMIN_EMAIL:process.env.ADMIN_EMAIL as string,
         ADMIN_PASSWORD:process.env.ADMIN_PASSWORD as string,
